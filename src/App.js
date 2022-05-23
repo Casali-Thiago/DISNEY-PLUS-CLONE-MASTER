ced50f79f6f248a9844b343e3c926797a73f6ff4
Casali-Thiago <thiago.balan14@gmail.com>
2022-05-23 18:11:41 -0300

    ADD VIEWER

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom'
 import MainPage from './pages/index.js'
+import Detail from './components/Detail'
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path="details" element={<Detail />} />
       </Routes>
     </Router>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Header from '../components/Header'
+
 import Home from '../components/Home'
 
 function index() {
   return (
     <div>
-      <Header />
       <Home />
     </div>
   )
